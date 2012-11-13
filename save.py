872be49c104b3284b6162c95e37da37c7231af60
U-Toph\Ted <Ted@Toph.(none)>
2012-11-13 01:46:26 -0800

    Initial wack at saving.

--- save.py
+++ save.py
@@ -4,6 +4,7 @@ import shlex
 
 class PrisonSave(dict):
   def __init__(self, filename):
+    self.__repr__ = printPrisonDict
     super(dict, self).__init__()
     with open(filename) as f:
       parser = TokenParser(self)
@@ -12,7 +13,23 @@ class PrisonSave(dict):
         for token in tokens:
           parser.parse(token)
 
-
+def printPrisonDict(self, depth = 0):
+  def printkey(key):
+    if ' ' in key:
+      key = '''"%s"''' % key
+    return key.ljust(19 + 2 * depth) + " "
+  objs = [] 
+  values = []
+  for key, value in self.iteritems():
+    if isinstance(value, dict):
+		objs.append(
+      '''BEGIN %s %s\n END\n''' % (printkey(key), 
+                                 printPrisonDict(value, depth + 1)) )
+    else:
+      values.append(printkey(key) + value)
+  return "\n".join(values) + ''.join(objs)
+      
+     
 class TokenParser:
   def __init__(self, manip):
     self.mode = self.NAME
@@ -47,5 +64,7 @@ class TokenParser:
     pass
   def OBJNAME():
     pass        
+
+      
 
 TESTFILE = "Prison Architect/saves/ted.prison"
